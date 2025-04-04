2c2ec063c14f025733ad4c9b5d348216e4356907
abduraw1d <abdurawidsadulaev@gmail.com>
2025-04-04 18:32:32 +0600

    rashid

--- kod.py
+++ kod.py
@@ -5,3 +5,10 @@ class Admin:
 
     def info(self):
         return f"Admin(name={self.name}, password={self.password})"
+
+class Rashid(Admin):
+    def __init__(self, name, password):
+        super().__init__(name, password)
+
+rashid = Rashid("Рашид", "12345")
+print(rashid.info())
